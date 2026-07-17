a9cb6aca73b1a7d1f2903cdc9b1d6c30d3377c1a
iammohitkutlehriaa <mohitrajput174307@gmail.com>
2026-07-17 11:46:03 +0530

    solved: butterfly,palindromic,rhombus,triangle pattern

---
+++ patterns/triangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    bool val = true;
+    int n =4;
+    for(int i = 0; i <= n; i++) {
+        for(int j =0; j <= i; j++) {
+            cout << val << " ";
+            val = !val;
+        }
+        cout << endl;
+    }
+    return 0;
+}
