4aa7f4dce849674c416a9a9b32c7c005046a4271
iammohitkutlehriaa <mohitrajput174307@gmail.com>
2026-07-22 19:59:56 +0530

    Arrays questions solved

---
+++ arrays/largest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main() {
+    int arr[] = {5,6,12,56,90,1};
+    int n = sizeof(arr) / sizeof(int);
+    int max = arr[0];
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << "Maximum value is : " << max << endl;
+    return 0;
+ }
