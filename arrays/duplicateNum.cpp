d1c8636855b327a13020ff43ff717bf484a080dd
iammohitkutlehriaa <mohitrajput174307@gmail.com>
2026-07-25 12:43:46 +0530

    question solved

---
+++ arrays/duplicateNum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+bool duplicate( int *arr , int n) {
+    for(int  i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+              return true;
+            }
+        }
+    }
+    return false;
+   
+}
+
+int main() {
+    int arr[5] = {1,2,3,1};
+    int n = sizeof(arr) / sizeof(int);
+    duplicate(arr , n);
+    return 0;
+}
