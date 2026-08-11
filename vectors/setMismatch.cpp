ebe6f948be80913809b1dc866f67517a5d461c7c
iammohitkutlehriaa <mohitrajput174307@gmail.com>
2026-08-11 21:32:00 +0530

    Better solution

--- vectors/setMismatch.cpp
+++ vectors/setMismatch.cpp
@@ -2,21 +2,31 @@
 #include<vector>
 using namespace std;
 
-vector<int> misMatch(vector<int> arr)
+vector<int> misMatch(vector<int>& nums)
 {
-    for(int  i = 1; i < arr.size(); i++) {
-        if(arr[i] == arr[i - 1]) {
-            int duplicate = arr[i];  
-        }
-        if(arr[i] > arr[i - 1] +1){
-            int missing = arr[i-1] + 1;
-            }
-    }
+    vector<int> ans;
+    int n  = nums.size();
+    vector<bool> isPresent(n+1, false);
 
+    for(int i = 0; i < n; i++) {
+        if(isPresent[nums[i]]) {
+            ans.push_back(nums[i]);
+        }
+        isPresent[nums[i]] = true;
+    }
+    for(int i = 1; i < isPresent.size(); i++) {
+        if(!isPresent[i]) {
+            ans.push_back(i);
+            break;
+        }
+    }
+    return ans;
 }
 
 int main() {
-    vector<int> arr = {1,2,2,4};
-    misMatch(arr);
+    vector<int> nums = {1,2,2,4};
+    vector<int> result = misMatch(nums);
+    cout << result[0] << " " << result[1];
+    return 0;
 
 }
