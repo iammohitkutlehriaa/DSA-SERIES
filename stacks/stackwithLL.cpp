431fe9bfe5d8dc249b39ad52153696adad9f72ab
iammohitkutlehriaa <mohitrajput174307@gmail.com>
2026-09-18 09:03:53 +0530

    stack with LL using class templates

---
+++ stacks/stackwithLL.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include<string>
+#include<list>
+using namespace std;
+
+template<class T>
+class Stack{
+    list<T>  ll;
+    public:
+
+    void push(T val) {
+        ll.push_front(val);
+    }
+    void pop() {
+        ll.pop_front();
+    }
+    T top() {
+        return ll.front();
+    }
+    bool isEmpty() {
+        return ll.size() == 0;
+    }
+};
+
+int main()
+{
+    Stack<int> s;
+    s.push(5);
+    s.push(4);
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    while(!s.isEmpty()) {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    return 0;
+}
